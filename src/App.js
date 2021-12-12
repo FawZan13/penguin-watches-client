3c389cb38f00001ce8ca5e4a64983b7372c48936
FawZan13 <fawzanbinmokhtar@gmail.com>
2021-12-12 23:43:13 +0600

    final

--- src/App.js
+++ src/App.js
@@ -15,44 +15,73 @@ import AuthProvider from './contexts/AuthProvider/AuthProvider';
 import PrivateRoute from './Pages/Login/PrivateRoute/PrivateRoute';
 import Purchase from './Pages/Purchase/Purchase/Purchase';
 import Dashboard from './Pages/Dashboard/Dashboard/Dashboard';
+import AddReview from './Pages/Dashboard/AddReview/AddReview';
+import MyOrders from './Pages/Dashboard/MyOrders/MyOrders';
+import Payment from './Pages/Dashboard/Payment/Payment';
+import ManageOrders from './Pages/Dashboard/ManageOrders/ManageOrders';
+import ManageProducts from './Pages/Dashboard/ManageProducts/ManageProducts';
+import MakeAdmin from './Pages/Dashboard/MakeAdmin/MakeAdmin';
+import AddProduct from './Pages/Dashboard/AddProduct/AddProduct';
 
 
 function App() {
   return (
-    <div className="App">
-      <AuthProvider>
-        <Router>
-          <Header></Header>
-          <Switch>
-            <Route path="/home">
-              <Home></Home>
-            </Route>
-            <Route path="/login">
-              <Login></Login>
-            </Route>
-            <Route path="/register">
-              <Register></Register>
-            </Route>
-            <Route exact path="/">
-              <Home></Home>
-            </Route>
-            <Route path="/explore">
-              <Explore></Explore>
-            </Route>
-            <PrivateRoute path="/purchase/:_id">
-              <Purchase></Purchase>
-            </PrivateRoute>
-            <PrivateRoute path="/dashboard">
-              <Dashboard></Dashboard>
-            </PrivateRoute>
-            <Route path="*">
-              <NotFound></NotFound>
-            </Route>
-          </Switch>
-          <Footer></Footer>
-        </Router>
-      </AuthProvider>
-    </div>
+
+    <AuthProvider>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/login">
+            <Login></Login>
+          </Route>
+          <Route exact path="/register">
+            <Register></Register>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/explore">
+            <Explore></Explore>
+          </Route>
+          <PrivateRoute exact path="/purchase/:_id">
+            <Purchase></Purchase>
+          </PrivateRoute>
+          <PrivateRoute path="/dashboard/manageOrders">
+            <ManageOrders></ManageOrders>
+          </PrivateRoute>
+          <PrivateRoute path="/dashboard/manageProducts">
+            <ManageProducts></ManageProducts>
+          </PrivateRoute>
+          <PrivateRoute path="/dashboard/makeAdmin">
+            <MakeAdmin></MakeAdmin>
+          </PrivateRoute>
+          <PrivateRoute path="/dashboard/addProduct">
+            <AddProduct></AddProduct>
+          </PrivateRoute>
+          <PrivateRoute path="/dashboard/addReview">
+            <AddReview></AddReview>
+          </PrivateRoute>
+          <PrivateRoute path="/dashboard/pay">
+            <Payment></Payment>
+          </PrivateRoute>
+          <Route path="/dashboard/myOrders">
+            <MyOrders></MyOrders>
+          </Route>
+          <PrivateRoute path="/dashboard">
+            <Dashboard></Dashboard>
+          </PrivateRoute>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+
+        </Switch>
+        <Footer></Footer>
+      </Router>
+    </AuthProvider>
+
   );
 }
 
--- src/Pages/Dashboard/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -1,177 +1,60 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
+import { Box, Button, CircularProgress } from '@mui/material';
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import {
-    BrowserRouter as
-        Switch,
-    Route,
-    Link,
-    useRouteMatch
-} from "react-router-dom";
-import AddReview from '../AddReview/AddReview';
-import Payment from '../Payment/Payment';
-import MyOrders from '../MyOrders/MyOrders';
-import MakeAdmin from '../MakeAdmin/MakeAdmin';
-import AddProduct from '../AddProduct/AddProduct';
-import ManageOrders from '../ManageOrders/ManageOrders';
+import { NavLink } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
-import AdminRoute from '../../Login/AdminRoute/AdminRoute';
-import PrivateRoute from '../../Login/PrivateRoute/PrivateRoute';
-import ManageProducts from '../ManageProducts/ManageProducts';
-import CircularProgress from '@mui/material/CircularProgress';
-import { Button } from '@mui/material';
 
-const drawerWidth = 200;
-
-function Dashboard(props) {
-    const { window } = props;
-    const [mobileOpen, setMobileOpen] = React.useState(false);
+const Dashboard = () => {
     const { admin, logout, isLoading } = useAuth();
-    let { path, url } = useRouteMatch();
+
     if (isLoading) {
         return <CircularProgress color="secondary" />
 
     }
-    const handleDrawerToggle = () => {
-        setMobileOpen(!mobileOpen);
-    };
-
-    const drawer = (
-        <div>
-            <Toolbar />
-            <Divider />
-            <Link style={{ textDecoration: 'none' }} to={`${url}`}>{admin ? <Button color="inherit">Make Admin</Button> : <Button color="inherit">Add Review</Button>}</Link>
-            {admin ? <Box>
-
-                <Link style={{ textDecoration: 'none' }} to={`${url}/addProduct`}><Button color="inherit">Add Product</Button></Link>
-                <Link style={{ textDecoration: 'none' }} to={`${url}/manageOrders`}><Button color="inherit">Manage Orders</Button></Link>
-                <Link style={{ textDecoration: 'none' }} to={`${url}/manageProducts`}><Button color="inherit">Manage Products</Button></Link>
-            </Box> :
-                <Box>
-                    {/* <Link style={{ textDecoration: 'none' }} to={`${url}`}><Button color="inherit">Add Review</Button></Link> <br></br> */}
-                    <Link style={{ textDecoration: 'none' }} to={`${url}/myOrders`}><Button color="inherit">My Orders</Button></Link><br></br>
-                    <Link style={{ textDecoration: 'none' }} to={`${url}/pay`}><Button color="inherit">Pay</Button></Link>
-                </Box>
-            }
-            <Button style={{ textDecoration: 'none' }} onClick={logout} color="inherit">Logout</Button>
-
-        </div >
-    );
-
-    const container = window !== undefined ? () => window().document.body : undefined;
-
     return (
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <AppBar
-                position="fixed"
-                sx={{
-                    width: { sm: `calc(100% - ${drawerWidth}px)` },
-                    ml: { sm: `${drawerWidth}px` },
-                }}
-            >
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                        onClick={handleDrawerToggle}
-                        sx={{ mr: 2, display: { sm: 'none' } }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" noWrap component="div">
-                        Dashboard
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Box
-                component="nav"
-                sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-                aria-label="mailbox folders"
-            >
-                <Drawer
-                    container={container}
-                    variant="temporary"
-                    open={mobileOpen}
-                    onClose={handleDrawerToggle}
-                    ModalProps={{
-                        keepMounted: true, // Better open performance on mobile.
-                    }}
-                    sx={{
-                        display: { xs: 'block', sm: 'none' },
-                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-                    }}
-                >
-                    {drawer}
-                </Drawer>
-                <Drawer
-                    variant="permanent"
-                    sx={{
-                        display: { xs: 'none', sm: 'block' },
-                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-                    }}
-                    open
-                >
-                    {drawer}
-                </Drawer>
-            </Box>
-            <Box
-                component="main"
-                sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
-            >
-                <Toolbar />
+        <div>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar color="secondary" position="static">
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            Dashboard
+                        </Typography>
+                        <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/home"><Button color="inherit">Home</Button></NavLink>
+                        {
+                            admin ?
+                                <Box>
+                                    <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/dashboard/makeAdmin"><Button color="inherit">Make Admin</Button></NavLink>
+                                    <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/dashboard/addProduct"><Button color="inherit">Add Product</Button></NavLink>
+                                    <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/dashboard/manageOrders"><Button color="inherit">Manage  Orders</Button></NavLink>
+                                    <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/dashboard/manageProducts"><Button color="inherit">Manage  Products</Button></NavLink>
+                                </Box>
+                                :
+                                <Box>
+                                    <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/dashboard/pay"><Button color="inherit">Pay</Button></NavLink>
+                                    <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/dashboard/addReview"><Button color="inherit">Add Review</Button></NavLink>
+                                    <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/dashboard/myOrders"><Button color="inherit">My Orders</Button></NavLink>
+                                </Box>
 
-                <Switch>
-                    <Route exact path={path}>
-                        {admin ? <MakeAdmin></MakeAdmin>
-                            :
-                            <AddReview></AddReview>
                         }
-                    </Route>
-                    <PrivateRoute path={`${path}/addReview`}>
-                        <AddReview></AddReview>
-                    </PrivateRoute>
-                    <PrivateRoute path={`${path}/pay`}>
-                        <Payment></Payment>
-                    </PrivateRoute>
-                    <PrivateRoute path={`${path}/myOrders`}>
-                        <MyOrders></MyOrders>
-                    </PrivateRoute>
-                    <AdminRoute path={`${path}/makeAdmin`}>
-                        <MakeAdmin></MakeAdmin>
-                    </AdminRoute>
-                    <AdminRoute path={`${path}/addProduct`}>
-                        <AddProduct></AddProduct>
-                    </AdminRoute>
-                    <AdminRoute path={`${path}/manageOrders`}>
-                        <ManageOrders></ManageOrders>
-                    </AdminRoute>
-                    <AdminRoute path={`${path}/manageProducts`}>
-                        <ManageProducts></ManageProducts>
-                    </AdminRoute>
-                </Switch>
-
-
+                    </Toolbar>
+                </AppBar>
             </Box>
-        </Box>
-    );
-}
 
-Dashboard.propTypes = {
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
-    window: PropTypes.func,
+        </div>
+    );
 };
 
 export default Dashboard;
